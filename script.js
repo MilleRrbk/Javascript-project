30646d7d09053facebf07cd2afdcbaace8b4c430
MilleRrbk <mile.rorbakhotmail.com>
2023-09-06 18:35:08 +0200

    nye knapper

---
+++ script.js
@@ -0,0 +1,11 @@
+const button1 = document.getElementById('Sød kartoffel');
+
+// Your condition (true or false)
+const condition = true; // Change this to your desired condition
+
+// Change the button color based on the condition
+if (condition) {
+  button.style.backgroundColor = 'green'; // Change to your desired color
+} else {
+  button.style.backgroundColor = 'red'; // Change to your desired color
+}
