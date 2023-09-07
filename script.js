33af0fc3f88d07dda2283a3d406a87d0ff9afdba
MilleRrbk <mile.rorbakhotmail.com>
2023-09-07 16:03:48 +0200

    alleknappervirker

--- script.js
+++ script.js
@@ -1,11 +1,112 @@
-const button1 = document.getElementById('Sød kartoffel');
+const sødKartoffel = document.querySelector('#button1');
+sødKartoffel.addEventListener('click', clickHandler);
 
-// Your condition (true or false)
-const condition = true; // Change this to your desired condition
-
-// Change the button color based on the condition
-if (condition) {
-  button.style.backgroundColor = 'green'; // Change to your desired color
-} else {
-  button.style.backgroundColor = 'red'; // Change to your desired color
+function clickHandler(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = 'rgb(120, 200, 40)';
 }
+
+
+const natsværmer = document.querySelector('#button2');
+natsværmer.addEventListener('click', clickHandler2);
+
+function clickHandler2(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = 'rgb(120, 200, 40)';
+}
+
+
+const kål = document.querySelector('#button3');
+kål.addEventListener('click', clickHandler3);
+
+function clickHandler3(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = 'rgb(120, 200, 40)';
+}
+
+
+const ananas = document.querySelector('#button4');
+ananas.addEventListener('click', clickHandler4);
+
+function clickHandler4(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = 'rgb(120, 200, 40)';
+}
+
+
+const honning = document.querySelector('#button5');
+honning.addEventListener('click', clickHandler5);
+
+function clickHandler5(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = 'rgb(120, 200, 40)';
+}
+
+
+const puppuccino = document.querySelector('#button9');
+puppuccino.addEventListener('click', clickHandler9);
+
+function clickHandler9(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = 'rgb(120, 200, 40)';
+}
+
+
+const chokolade = document.querySelector('#button6');
+chokolade.addEventListener('click', clickHandler6);
+
+function clickHandler6(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = ('red');
+}
+
+
+const vindruer = document.querySelector('#button7');
+vindruer.addEventListener('click', clickHandler7);
+
+function clickHandler7(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = ('red');
+}
+
+const avocado = document.querySelector('#button8');
+avocado.addEventListener('click', clickHandler8);
+
+function clickHandler8(evt){
+  console.log(evt.target);
+  evt.target.style.backgroundColor = ('red');
+}
+
+const spangsbergFlødeboller = document.querySelector('#button10');
+spangsbergFlødeboller.addEventListener('click', clickHandler10);
+
+function clickHandler10(evt) {
+  console.log(evt.target);
+  evt.target.style.backgroundColor = ('red');
+}
+
+
+// document.getElementById('button1') = true
+
+// console.log(sødKartoffel)
+
+
+
+
+// const buttonsAll = document.querySelectorAll('.buttons');
+//   console.log(buttonsAll)
+
+// const correctAnswers = ['#button1', '#button2']
+
+// if(correctAnswers === true) {
+//   sødKartoffel.addEventListener('click', clickHandler);
+
+//   function clickHandler(evt) {
+//   console.log(evt.target);
+//   evt.target.style.backgroundColor = 'rgb(120, 200, 40)'
+// }
+
+// }
+
+
+// document.querySelectorAll('.buttons'),
